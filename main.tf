f53e1261ba9ded3fbe8cdc1cd6b510f8b0cb5631
aamer <aamer@Ameers-MacBook-Air.local>
2023-07-30 20:45:36 +0530

    Alhumdulilllah.. all installed and Updated

--- main.tf
+++ main.tf
@@ -86,3 +86,116 @@ module "eks" {
   }
 
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+module "cert_manager_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5"
+
+  role_name                     = "cert-manager"
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = ["arn:aws:route53:::hostedzone/Z0411043ISXL599QGTOU"]
+
+  oidc_providers = {
+    eks = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cert-manager"]
+    }
+  }
+}
+
+module "external_dns_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5"
+
+  role_name                     = "external-dns"
+  attach_external_dns_policy    = true
+  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/Z0411043ISXL599QGTOU"]
+
+  oidc_providers = {
+    eks = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+}
+
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace  = "cert-manager"
+
+  create_namespace = false
+
+  set {
+    name  = "wait-for"
+    value = module.cert_manager_irsa_role.iam_role_arn
+  }
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com\\/role-arn"
+    value = module.cert_manager_irsa_role.iam_role_arn
+  }
+
+  values = [
+    "${file("modules/helm/templates/values-cert-manager.yaml")}"
+  ]
+}
+
+resource "helm_release" "external-dns" {
+  name       = "external-dns"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "external-dns"
+  namespace  = "kube-system"
+
+  create_namespace = false
+
+  set {
+    name  = "wait-for"
+    value = module.external_dns_irsa_role.iam_role_arn
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com\\/role-arn"
+    value = module.external_dns_irsa_role.iam_role_arn
+  }
+
+  set {
+    name  = "policy"
+    value = "sync"
+  }
+
+  values = [
+    "${file("modules/helm/templates/values-external-dns.yaml")}"
+  ]
+}
+
+resource "helm_release" "nginx-ingress" {
+  name       = "nginx-ingress"
+  repository = "https://helm.nginx.com/stable"
+  chart      = "nginx-ingress"
+  namespace  = "ingress"
+
+  create_namespace = false
+}
